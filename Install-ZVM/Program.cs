dcad6517173375a384c0c336006d9fd19f73187c
Nir Orgad <nir.orgad@zerto.com>
2019-04-11 18:42:50 +0300

    Add project files.

---
+++ Install-ZVM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Install_ZVM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
