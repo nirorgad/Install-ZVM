e7e0e521cd0776a25653e668fe2a682b5143676f
Nir Orgad <nir.orgad@zerto.com>
2019-05-13 11:05:13 +0300

    full sync

--- Install-ZVM/Program.cs
+++ Install-ZVM/Program.cs
@@ -4,12 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Diagnostics;
+using System.IO;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
 namespace Install_ZVM
 {
     class Program
     {
-        static void Main(string[] args)
+        static void login(IWebDriver driver, string UserName, string Password)
         {
+
+            var UserNameField = driver.FindElement(By.Id("usernameInput"));
+            UserNameField.SendKeys(UserName);
+            var PasswordField = driver.FindElement(By.Id("passwordInput"));
+            PasswordField.SendKeys(Password);
+            PasswordField.Submit();
         }
-    }
+         static void Main(string[] args)
+        {
+                string SiteIp = "";
+                string UserName = "administrator";
+                string Password = "zertodata";
+
+                Console.WriteLine("Please prvoide the ZVM IP address");
+                SiteIp = Console.ReadLine();
+                Console.WriteLine("Please prvoide the ZVM User Name ");
+                UserName = Console.ReadLine();
+                Console.WriteLine("Please prvoide the ZVM Password");
+                Password = Console.ReadLine();
+
+                var URL = "https://" + SiteIp + ":9669";
+
+                IWebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();        
+                driver.Url = URL;
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+                login(driver, UserName, Password);
+
+            
+
+                driver.Close();
+
+            }
+        }
 }
