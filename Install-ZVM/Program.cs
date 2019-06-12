04bccc3ad085ace15c2e282c4d875795a92c8077
Nir Orgad <nir.orgad@zerto.com>
2019-06-12 16:51:32 +0300

    enter license

--- Install-ZVM/Program.cs
+++ Install-ZVM/Program.cs
@@ -18,38 +18,55 @@ namespace Install_ZVM
         static void login(IWebDriver driver, string UserName, string Password)
         {
 
-            var UserNameField = driver.FindElement(By.Id("usernameInput"));
+            var UserNameField = driver.FindElement(By.Id("userNameInput"));
             UserNameField.SendKeys(UserName);
             var PasswordField = driver.FindElement(By.Id("passwordInput"));
             PasswordField.SendKeys(Password);
             PasswordField.Submit();
+
+         
         }
-         static void Main(string[] args)
+
+        static void Enterlicense(IWebDriver driver, string License )
+
         {
-                string SiteIp = "";
-                string UserName = "administrator";
-                string Password = "zertodata";
+          try
+            {
+                var Checker = driver.FindElement(By.XPath("//*[@ng-model='values.license']"));
+                Checker.SendKeys(License);
+                Console.WriteLine("License enterd");
+                Checker.Submit();
+             }
+          catch
+            {
+                Console.WriteLine("License all redy exist");
+            }    
 
 
-            d
-
-                Console.WriteLine("Please prvoide the ZVM IP address");
+        }
+        static void Main(string[] args)
+        {
+            string SiteIp = "10.171.16.76";
+            string UserName = "root";
+            string Password = "Zertodata1!";
+            string License = "PXYB5WXKY6MQXF59BQMJVCFKRR43AEFU55TLB6YWNZ";
+           /* Console.WriteLine("Please prvoide the ZVM IP address");
                 SiteIp = Console.ReadLine();
                 Console.WriteLine("Please prvoide the ZVM User Name ");
                 UserName = Console.ReadLine();
                 Console.WriteLine("Please prvoide the ZVM Password");
                 Password = Console.ReadLine();
-
-                var URL = "https://" + SiteIp + ":9669";
+           */
+            var URL = "https://" + SiteIp + ":9669";
 
                 IWebDriver driver = new ChromeDriver();
                 driver.Manage().Window.Maximize();        
                 driver.Url = URL;
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
                 login(driver, UserName, Password);
+                Enterlicense(driver, License);
 
-            
 
+            Console.ReadKey();
                 driver.Close();
 
             }
