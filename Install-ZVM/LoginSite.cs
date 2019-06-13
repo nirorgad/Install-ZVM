b429425fe5dd34698c42523b789edb62919d15a0
Nir Orgad <nir.orgad@zerto.com>
2019-06-13 14:46:20 +0300

    Rearange class and add Site1 var

---
+++ Install-ZVM/LoginSite.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace Install_ZVM
+{
+    class LoginSite
+    {
+        public void Login(IWebDriver driver, string UserName, string Password)
+        {
+
+            var UserNameField = driver.FindElement(By.Id("userNameInput"));
+            UserNameField.SendKeys(UserName);
+            var PasswordField = driver.FindElement(By.Id("passwordInput"));
+            PasswordField.SendKeys(Password);
+            PasswordField.Submit();
+
+
+        }
+
+        public void Enterlicense(IWebDriver driver, string License)
+
+        {
+            try
+            {
+                var Checker = driver.FindElement(By.XPath("//*[@ng-model='values.license']"));
+                Checker.SendKeys(License);
+                Console.WriteLine("License enterd");
+                Checker.Submit();
+            }
+            catch
+            {
+                Console.WriteLine("License all redy exist");
+            }
+
+
+        }
+
+
+    }
+}
--- Install-ZVM/Program.cs
+++ Install-ZVM/Program.cs
@@ -1,73 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.Diagnostics;
-using System.IO;
-
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 
 namespace Install_ZVM
 {
     class Program
     {
-        static void login(IWebDriver driver, string UserName, string Password)
-        {
-
-            var UserNameField = driver.FindElement(By.Id("userNameInput"));
-            UserNameField.SendKeys(UserName);
-            var PasswordField = driver.FindElement(By.Id("passwordInput"));
-            PasswordField.SendKeys(Password);
-            PasswordField.Submit();
-
-         
-        }
-
-        static void Enterlicense(IWebDriver driver, string License )
-
-        {
-          try
-            {
-                var Checker = driver.FindElement(By.XPath("//*[@ng-model='values.license']"));
-                Checker.SendKeys(License);
-                Console.WriteLine("License enterd");
-                Checker.Submit();
-             }
-          catch
-            {
-                Console.WriteLine("License all redy exist");
-            }    
-
-
-        }
+      
         static void Main(string[] args)
         {
             string SiteIp = "10.171.16.76";
             string UserName = "root";
             string Password = "Zertodata1!";
             string License = "PXYB5WXKY6MQXF59BQMJVCFKRR43AEFU55TLB6YWNZ";
-           /* Console.WriteLine("Please prvoide the ZVM IP address");
-                SiteIp = Console.ReadLine();
-                Console.WriteLine("Please prvoide the ZVM User Name ");
-                UserName = Console.ReadLine();
-                Console.WriteLine("Please prvoide the ZVM Password");
-                Password = Console.ReadLine();
-           */
             var URL = "https://" + SiteIp + ":9669";
 
-                IWebDriver driver = new ChromeDriver();
-                driver.Manage().Window.Maximize();        
-                driver.Url = URL;
-                login(driver, UserName, Password);
-                Enterlicense(driver, License);
 
+            /* Console.WriteLine("Please prvoide the ZVM IP address");
+                 SiteIp = Console.ReadLine();
+                 Console.WriteLine("Please prvoide the ZVM User Name ");
+                 UserName = Console.ReadLine();
+                 Console.WriteLine("Please prvoide the ZVM Password");
+                 Password = Console.ReadLine();
+            */
+
+
+
+            //Open Chrome and enter ZVM GUI //
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();        
+            driver.Url = URL;
+
+
+            //Loging to GUI//
+            LoginSite Site1 = new LoginSite();
+            Site1.Login(driver, UserName, Password);
+            Site1.Enterlicense(driver, License);
 
             Console.ReadKey();
-                driver.Close();
+            driver.Close();
 
             }
         }
